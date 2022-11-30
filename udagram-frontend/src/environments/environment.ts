8eb6a1107d341a300bb207eeac2005312d9bc464
ebukachuqz <ebukachuqz@gmail.com>
2022-11-30 18:48:43 +0100

    Replaced localhost with external IP of reverseproxy on frontend and updated CI file for new frontend tag

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -4,8 +4,9 @@
 
 export const environment = {
   production: false,
-  appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  appName: "Udagram",
+  apiHost:
+    "http://a012c1b57039a4410960a87166bc1a63-141407646.us-east-1.elb.amazonaws.com:8080/api/v0",
 };
 
 /*
